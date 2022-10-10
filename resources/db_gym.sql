8c0fa6d473374eca2727af83699bc00d283eacae
tonymg1 <tonymartingaldeano@gmail.com>
2022-10-10 13:33:05 +0200

    new db

--- resources/db_gym.sql
+++ resources/db_gym.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 10-10-2022 a las 12:04:35
+-- Tiempo de generación: 10-10-2022 a las 13:31:46
 -- Versión del servidor: 10.4.17-MariaDB
 -- Versión de PHP: 7.2.34
 
@@ -32,7 +32,7 @@ CREATE TABLE `members` (
   `name` varchar(25) NOT NULL,
   `last_name` varchar(25) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `password` varchar(80) NOT NULL,
   `sport_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -41,7 +41,11 @@ CREATE TABLE `members` (
 --
 
 INSERT INTO `members` (`id`, `name`, `last_name`, `email`, `password`, `sport_id`) VALUES
-(1, '', '', '', '', NULL);
+(1, 'Rafael', 'Nadal', 'rafanadal@gmail.com', '$2y$10$O9j0Z6TrBo35bDPRG6E8IOF9Cy8NqfKW26jamsgBcDYrFavbGid5W', 3),
+(2, 'Fernando ', 'Alonso', 'falonso@gmail.com', '$2y$10$Yy/UueX.RDDzCH7aHheUDejGqdP16URdvkVIs/EY5ftPnAk.lQl7q', 2),
+(3, 'Arnold', 'Schwarzenegger', 'chuache@gmail.com', '$2y$10$NBZA7uer4zmj36k9e7IrYekfApQh387jEvNr6Y9W78h2FpLzyzre6', 1),
+(4, 'Muhammad', 'Ali', 'ali@gmail.com', '$2y$10$xg.TgxU.eEShR1grXbn6IO0he.Hy5aWwIQzrfF0IyMkYZ9U31woQG', 2),
+(5, 'Carolina', 'Marín', 'cmarin@gmail.com', '$2y$10$JgLjTjoch/Ha15ZZKeCVGOiglc6z79V79wZwSH74PbiwnirniN5e2', 3);
 
 -- --------------------------------------------------------
 
@@ -53,6 +57,15 @@ CREATE TABLE `sports` (
   `id` int(11) NOT NULL,
   `sport` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `sports`
+--
+
+INSERT INTO `sports` (`id`, `sport`) VALUES
+(1, 'Pilates'),
+(2, 'Powerlifiting'),
+(3, 'Padel');
 
 --
 -- Índices para tablas volcadas
@@ -79,13 +92,13 @@ ALTER TABLE `sports`
 -- AUTO_INCREMENT de la tabla `members`
 --
 ALTER TABLE `members`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `sports`
 --
 ALTER TABLE `sports`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Restricciones para tablas volcadas
