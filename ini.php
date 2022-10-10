14cb49d5a308d57a9fd20eac221cc58fd3f55c3c
Pau Civill <pau.civill@gmail.com>
2022-10-10 15:55:12 +0200

    password hash and verify

--- ini.php
+++ ini.php
@@ -1,8 +1,1 @@
 <?php
-
-// $pwdInput = "c123";
-// $pwdHashedInDb = password_hash($pwdInput, PASSWORD_DEFAULT);
-// echo $pwdHashedInDb. "<br>";
-
-// $pwdVerify = password_verify($pwdInput, $pwdHashedInDb);
-// echo $pwdVerify . " incorrect";
---
+++ models/LoginModel.php
@@ -0,0 +1,8 @@
+<?php
+
+// $pwdInput = "c123";
+// $pwdHashedInDb = password_hash($pwdInput, PASSWORD_DEFAULT);
+// echo $pwdHashedInDb. "<br>";
+
+// $pwdVerify = password_verify($pwdInput, $pwdHashedInDb);
+// echo $pwdVerify . " incorrect";
