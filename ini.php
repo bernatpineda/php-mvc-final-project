0f09eb7c7ddd3ef01c4d0e8d90ff6b4491426bd7
bernat <bernatpinedatorrens@gmail.com>
2022-10-11 11:32:09 +0200

    login-bootstrap

--- ini.php
+++ ini.php
@@ -1,1 +1,13 @@
 <?php
+require_once "config/constants.php";
+require_once "config/db.php";
+require_once "core/classes/Database.php";
+require_once "core/classes/Model.php";
+require_once "core/classes/View.php";
+require_once "core/classes/Controller.php";
+
+require_once "core/Router.php";
+//require_once "core/login/login.php";
+$email = $_POST['email'];
+echo $email;
+$password = $_POST['password'];
