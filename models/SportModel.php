f6bc58776b53749ba018c8b5294597d254d7b73d
Elirv <elizabeth.ramos101@hotmail.com>
2022-10-14 12:31:59 +0100

    id

--- models/SportModel.php
+++ models/SportModel.php
@@ -11,7 +11,7 @@ class SportModel {
 
     function get_sports(){
         
-        $query = $this->db->connect()->prepare("select count(members.id), sports.sport from sports right join members on members.sport_id = sports.id group by members.sport_id;");
+        $query = $this->db->connect()->prepare("select count(members.id), sports.sport, sports.id from sports right join members on members.sport_id = sports.id group by members.sport_id;");
         try {
             $query->execute(); // lanza la petición del prepare a la base de datos
             $sports = $query->fetchAll();
